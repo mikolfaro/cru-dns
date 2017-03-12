88a28405def082d72955826626f631d045eba825
Mikol Faro <mikol.faro@gmail.com>
2017-03-12 15:26:56 +0100

    Add validation errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def pretty_bool(bool)
     bool ? '✔' : '✘'
   end
+
+  def error_block(model, field)
+    errors = model.errors[field]
+    return '' if errors.empty?
+
+    p_tags = errors.full_messages.map do |msg|
+      content_tag p, msg, class: 'help-block'
+    end
+
+    p_tags.join
+  end
 end
