f21bd60edca7fc1703b2a2a1f4c2c0983f9e51c4
Mikol Faro <mikol.faro@gmail.com>
2017-03-12 09:35:36 +0100

    Record should not be abstract to sue STI

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,6 @@
 class Record < ActiveRecord::Base
   self.table_name = 'records'
-  self.abstract_class = true
 
-  validates_presence_of :type, :name, :value
+  validates_presence_of :name, :value
   validates_uniqueness_of :name, scope: :type
 end
